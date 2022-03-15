2a6ba57085134d1a595405fa877ad67a2cc4fb5d
thngph <19520981@gm.uit.edu.vn>
2022-03-15 22:33:29 +0700

    update

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, flash, jsonify
+from flask import Flask, request, redirect, url_for, flash, jsonify
 import numpy as np
 import pickle as p
 import json
@@ -6,6 +6,9 @@ import json
 
 app = Flask(__name__)
 
+@app.route("/", methods=["GET"])
+def hello():
+    return jsonify("hello from ML API of Red Wine data!")
 
 @app.route('/api/', methods=['POST'])
 def makecalc():
@@ -15,6 +18,6 @@ def makecalc():
     return jsonify(prediction)
 
 if __name__ == '__main__':
-    modelfile = 'models/final_prediction.pickle'
+    modelfile = 'models/final_prediction.sav'
     model = p.load(open(modelfile, 'rb'))
     app.run(debug=True, host='0.0.0.0')
